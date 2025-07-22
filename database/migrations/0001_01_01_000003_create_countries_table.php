4da5e18d997019e3109a1ababee0a47b4e02d1e8
Leandro <leandro.machado@cgm.fortaleza.ce.gov.br>
2025-07-22 16:36:23 -0300

    salvando

---
+++ database/migrations/0001_01_01_000003_create_countries_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->ulid('id');
+            $table->string('name', 255);
+            $table->string('code', 2)->unique();
+            $table->string('phone_code', 10)->nullable();
+            $table->string('currency', 10)->nullable();
+            $table->text('flag')->nullable();
+            $table->boolean('active')->default(true);
+            $table->foreignUlid('created_by')->references('id')->on('users')->restrictOnDelete();
+            $table->foreignUlid('updated_by')->references('id')->on('users')->restrictOnDelete();
+            $table->foreignUlid('deleted_by')->references('id')->on('users')->restrictOnDelete();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('countries');
+    }
+};
